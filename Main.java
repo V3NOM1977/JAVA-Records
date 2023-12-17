b4bda3075fa1b50ac0fcddea88360a42531d2ec6
V3NOM1977 <rishabhrawat1977@gmail.com>
2023-12-17 21:57:56 +0530

    Documentation

--- Main.java
+++ Main.java
@@ -1,5 +1,17 @@
 public class Main {
 
+    /**
+     * 1. Introduced in java 14.
+     * 2. Used as data carring objects.
+     * 3. Consist one or more data field with final litral.
+     * 4. Auto generate getters, hashcode, equals, toString methods.
+     * 5. Reords are final.
+     * 6. Can have multi constructors.
+     * 7. Can have instance and staic methods.
+     * 8. Records are immutable in nature.
+     * 
+     * @param args
+     */
     public static void main(String[] args) {
         var user1 = new User(2, "Rishabh Rawat", "abc");
         System.out.println(user1);
@@ -15,5 +27,5 @@ public class Main {
         user3.instanceMethod();
         User.classMethod();
     }
-
+    
 }
